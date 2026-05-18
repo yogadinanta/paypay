6259ea9ceee93e8d1a572e95c59d1e6d37c1f3a8
yogadinanta <yogadinanta@gmail.com>
2026-05-18 10:44:17 +0800

    feat: building new user API

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
 const express = require('express');
-
 const sequelize = require('./config/db');
-
 const db = require('./models');
-
-
 
 const app = express();
 
 app.use(express.json());
 
+// TEST ROUTE
+app.get('/', (req, res) => {
+  res.send('API aktif!');
+});
 
-app.get('/', (req, res) => res.send('API aktif!'));
+// ======================
+// ROUTES (INTEGRASI API)
+// ======================
+const userRoutes = require('./routes/user.route');
+const productRoutes = require('./routes/product.route.js');
+const transactionRoutes = require('./routes/transaction.route');
 
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/transactions', transactionRoutes);
 
-// tambahkan routing kamu dibawah sini
-
-
-
-// Koneksi ke database
-
+// ======================
+// DATABASE CONNECTION
+// ======================
 sequelize.authenticate()
-
   .then(() => {
-
     console.log('✅ Terkoneksi ke MySQL');
-
     return db.sequelize.sync({ alter: true });
-
   })
-
   .then(() => {
-
     console.log('✅ Sinkronisasi selesai');
-
   })
-
   .catch((err) => {
-
     console.error('❌ Gagal konek:', err);
-
   });
 
-
-// Jalankan server
-
+// ======================
+// RUN SERVER
+// ======================
 app.listen(3000, () => {
-
   console.log('🚀 Server jalan di http://localhost:3000');
-
 });
